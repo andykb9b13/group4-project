78835f857928edc1a7496a26517b00157fee2428
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-03-09 19:01:31 -0500

    debugged seeds and models

--- seeds/userAccountData.js
+++ seeds/userAccountData.js
@@ -1,15 +1,14 @@
-const { User } = require('../models');
+const { User } = require("../models");
 
 const userdata = [
-    {
+  {
     user_id: "1",
     name: "Jane Doe",
     email: "jane@thedoe.com",
-    password: "d0th3d03"
-    }
-]
+    password: "d0th3d03",
+  },
+];
 
-
-const seedUsers = () => User.bulkcreate(userdata);
+const seedUsers = () => User.bulkCreate(userdata);
 
 module.exports = seedUsers;
