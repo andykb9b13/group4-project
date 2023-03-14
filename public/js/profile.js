1fa6ecb21b4ae2bb8114f52a92739f8641d4c427
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-03-13 22:24:00 -0400

    added logout functionality

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,1 +1,17 @@
 // js file for manipulating elements on profile page
+
+const logout = async () => {
+  try {
+    const response = await fetch("api/user/logout", {
+      method: "POST",
+    });
+    if (response.ok) {
+      document.location.replace("/login");
+      alert("You are now logged out");
+    }
+  } catch (err) {
+    alert("Couldn't log out!");
+  }
+};
+
+document.querySelector("#logoutBtn").addEventListener("click", logout);
